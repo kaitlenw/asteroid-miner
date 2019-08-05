cc905a31e528ee86a02799572c3a16c489e65913
Kaitlen <kaitlenw@outlook.com>
2019-08-05 20:54:38 +0100

    Assets - Scripts
    - code to produce movement of the ship, and infinite scrolling effect for the background

---
+++ Assets/Scripts/FollowUV.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowUV : MonoBehaviour {
+
+	public float parallax = 2f;
+
+	void Update () {
+
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x = transform.position.x / transform.localScale.x / parallax;
+		offset.y = transform.position.y / transform.localScale.y / parallax;
+
+		mat.mainTextureOffset = offset;
+
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float turnSpeed = 2.5f;
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+
+    void FixedUpdate ()
+    {
+        float x = Input.GetAxis ("Horizontal");
+        float y = Input.GetAxis ("Vertical");
+
+        Vector2 moveVec = new Vector2 (x, y) * speed;
+        GetComponent<Rigidbody2D> ().AddForce (moveVec);
+
+        if (moveVec != Vector2.zero)
+        {
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (Vector3.forward, moveVec), Time.fixedDeltaTime * turnSpeed);
+        }
+    }
+}
