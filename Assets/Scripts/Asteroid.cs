0eba00dc13df9edcf1cbec2c055cb9d9a09aba96
Kaitlen <kaitlenw@outlook.com>
2019-08-10 00:21:08 +0100

    Removed collisions between player and explosions, added material drops
    - explosions are no longer solid
    - added code to Asteroid.cs to allow the asteroids to drop a material when they are destroyed; could be changed in future to be a randomly generated material but for now, it is set as a public variable to the script for each asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -8,6 +8,8 @@ public class Asteroid : MonoBehaviour
     public SpriteRenderer explosion;
     private AudioSource audioSource;
     public AudioClip explodeAsteroid;
+
+    public Rigidbody2D material;
     
     void Start()
     {
@@ -20,8 +22,13 @@ public class Asteroid : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().enabled = false;
             explosion.enabled = true;
+            gameObject.GetComponent<Collider2D>().enabled = false;
             Destroy(collision.gameObject);
             audioSource.PlayOneShot(explodeAsteroid, 0.7f);
+            if (material != null)
+            {
+                Instantiate(material, transform.position, transform.rotation);
+            }
             Destroy(gameObject, 0.8f);
         }
     }
