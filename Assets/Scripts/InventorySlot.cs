0afab367aef9161503f8b3eca5edbbf9dc562e1b
Kaitlen <kaitlenw@outlook.com>
2019-12-16 20:21:28 +0000

    Fixed missing text from gold description in inventory window

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -10,12 +11,16 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     public Image icon;
     public Text itemCount;
 
-    public Text description;
+    public GameObject description;
     private Inventory inventory;
+
+    private TMP_Text descText;
 
     void Start()
     {
         inventory = Inventory.instance;
+        descText = description.transform.Find("DescriptionText").gameObject.GetComponent<TMP_Text>();
+        descText.text = $"Inventory\n\nTotal number of Items: {inventory.Count()}\nCurrent Capacity: {inventory.inventoryCapacity}";
     }
     void OnEnable()
     {
@@ -23,16 +28,16 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         icon.enabled = true;
         itemCount.text = Inventory.instance.inventory[item.id] + "";
         itemCount.enabled = true;
+        descText.text = $"Inventory\n\nTotal number of Items: {inventory.Count()}\nCurrent Capacity: {inventory.inventoryCapacity}";
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        description.text = item.itemName + "\n\n" + item.description + "\nSelling Price: " + item.sellingPrice;
+        descText.text = item.itemName + "\n\n" + item.description + "\nSelling Price: " + item.sellingPrice;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Debug.Log("Mouse exit");
-        description.text = $"Inventory\n\nTotal number of Items: {inventory.Count()}\nCurrent Capacity: {inventory.inventoryCapacity}";
+        descText.text = $"Inventory\n\nTotal number of Items: {inventory.Count()}\nCurrent Capacity: {inventory.inventoryCapacity}";
     }
 }
